b52440f4b3cb60cba7fd5dc1715fc5d17afcde0e
Jeffrey Ho <jeffreyhodev@gmail.com>
2025-06-01 23:29:11 +0800

    added endpoints for register, save questions, login, get history list and history details for v1.1

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ const knex = require('knex')({
 app.use(cors({
   origin: ['http://localhost:3000' , 'https://gcp-exam-questions.vercel.app']
 }));
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
 
@@ -53,6 +54,133 @@ app.get('/', (req, res) => {
 
 })
 
+app.post('/get_history_list', (req, res) => {
+  const { identifier } = req.body;
+  try {
+    knex.raw(`SELECT * FROM login_identifier WHERE identifier='${identifier}'`)
+    .then(response => {
+      let user = response.rows[0];
+      if (!user) {
+        return res.status(404).json({ error: 'Identifier not found' });
+      }else {
+        knex.raw(`SELECT * FROM history WHERE identifier_id=${user.id} ORDER BY saved_time DESC`)
+        .then(response => {
+          res.json(response.rows);
+        })
+        .catch(err => {
+          console.error('Error fetching history:', err);
+          res.status(500).json({ error: err });
+        })
+      }
+    })
+    .catch(err => {
+      console.error('Error checking identifier:', err);
+      res.status(500).json({ error: err });
+    })
+  }catch(err) {
+    console.error('Error fetching history:', err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+})
+
+app.post('/save_questions', (req, res) => {
+  const { identifier, questions, scoreResult } = req.body;
+  try {
+    knex.raw(`SELECT * FROM login_identifier WHERE identifier='${identifier}'`)
+    .then(response => {
+      let user = response.rows[0];
+      if (!user) {
+        return res.status(404).json({ error: 'Identifier not found' });
+      }else {
+        knex.raw(
+          `INSERT INTO history (identifier_id, questions, saved_time, score) VALUES (?, ?, NOW() AT TIME ZONE 'Asia/Singapore', ?)`,
+          [user.id, JSON.stringify(questions), scoreResult]
+        )
+        .then(response => {
+          res.json({ message: 'Questions saved successfully', details: response });
+        })
+        .catch(err => {
+          console.error('Error saving questions:', err);
+          res.status(500).json({ error: err });
+        })
+      }
+    })
+    .catch(err => {
+      console.error('Error checking identifier:', err);
+      res.json({ error: err });
+    })
+  }catch(err) {
+    console.error('Error saving questions:', err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+
+
+})
+
+app.post('/register_identifier', (req, res) => {
+  try {
+    const { identifier, registerEmail } = req.body;
+    knex.raw(`INSERT INTO login_identifier (identifier, email, created) VALUES ('${identifier}', '${registerEmail}', NOW() AT TIME ZONE 'Asia/Singapore') ON CONFLICT (identifier) DO NOTHING`)
+    .then(response => {
+      res.json({ message: 'Identifier registered successfully', details: response });
+    })
+    .catch(err => {
+      console.error('Error registering identifier:', err);
+      res.status(500).json({ error: err });
+    })
+  }catch(err) {
+    console.error('Error registering identifier:', err);
+    res.status(500).json({ error: err });
+  }
+})
+
+
+app.post(`/login`, (req, res) => {
+  const { identifier } = req.body;
+  try {
+    knex.raw(`SELECT * FROM login_identifier WHERE identifier='${identifier}'`)
+    .then(response => {
+      if (response.rows.length > 0) {
+        res.json({ message: 'Login successful', details: response.rows[0]});
+      } else {
+        res.status(404).json({ error: 'Identifier not found' });
+      }
+    })
+    .catch(err => {
+      console.error('Error logging in:', err);
+      res.status(500).json({ error: err });
+    })
+  }catch(err) {
+    console.error('Error logging in:', err);
+    res.status(500).json({ error: err });
+  }
+})
+
+
+app.post('/historyDetails', (req, res) => {
+  const { identifier, row_id } = req.body;
+  try {
+    knex.raw(`SELECT h.*
+      FROM history h
+      JOIN login_identifier u ON h.identifier_id=u.id
+      WHERE u.identifier='${identifier}' AND h.id=${row_id}`)
+    .then(response => {
+      if (response.rows.length > 0) {
+        res.json(response.rows[0]);
+      } else {
+        res.status(404).json({ error: 'History detail not found' });
+      }
+    })
+    .catch(err => {
+      console.error('Error fetching history detail:', err);
+      res.status(500).json({ error: err });
+    })
+  }catch(err) {
+    console.error('Error fetching history detail:', err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`GCP-Exam Backend Service listening on port ${port}`)
 })
